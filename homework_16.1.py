6f03537e80b56b20eaa058b14f19d0e4641d6d3c
anzhelikabilytska <165314668+anzhelikabilytska@users.noreply.github.com>
2024-11-03 20:50:19 +0000

    done homework and test

---
+++ homework_16.1.py
@@ -0,0 +1,20 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+class Manager(Employee):
+    def __init__(self, name, salary, department):
+        super().__init__(name, salary)
+        self.department = department
+
+class Developer(Employee):
+    def __init__(self, name, salary, programming_language):
+        super().__init__(name, salary)
+        self.programming_language = programming_language
+
+class TeamLead(Manager, Developer):
+    def __init__(self, name, salary, department, programming_language, team_size):
+        Manager.__init__(self, name, salary, department)
+        Developer.__init__(self, name, salary, programming_language)
+        self.team_size = team_size
